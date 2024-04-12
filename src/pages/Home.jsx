a42ca3b2eb061ef493607cb9907338f9e73f7988
adnan2508 <adnandiu2800@gmail.com>
2024-04-12 20:45:03 +0600

    navbar done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
     return (
         <div>
+            <Navbar/>
             <h2 className='text-7xl text-slate-950'>This is HOME</h2>
         </div>
     );
