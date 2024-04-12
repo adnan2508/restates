2cbb44d4385684c06862bd35ee13ba55e285ad9b
adnan2508 <adnandiu2800@gmail.com>
2024-04-12 20:56:40 +0600

    footer done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-base-100 w-11/12 mx-auto">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -23,10 +23,12 @@ const Navbar = () => {
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Login</a>
+  <a className="btn">Login</a>
+  </div>
+    
   </div>
 </div>
-        </div>
+
     );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,14 @@ import Navbar from '../components/Navbar';
 
 const Home = () => {
     return (
-        <div>
-            <Navbar/>
+        <div className='w-11/12 mx-auto'>
+            <h2 className='text-7xl text-slate-950'>This is HOME</h2>
+            <h2 className='text-7xl text-slate-950'>This is HOME</h2>
+            <h2 className='text-7xl text-slate-950'>This is HOME</h2>
+            <h2 className='text-7xl text-slate-950'>This is HOME</h2>
+            <h2 className='text-7xl text-slate-950'>This is HOME</h2>
+            <h2 className='text-7xl text-slate-950'>This is HOME</h2>
+            <h2 className='text-7xl text-slate-950'>This is HOME</h2>
             <h2 className='text-7xl text-slate-950'>This is HOME</h2>
         </div>
     );
