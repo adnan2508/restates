a22a7b36f149357d141df3202bf2ba1f49e6c3cb
adnan2508 <adnandiu2800@gmail.com>
2024-04-13 02:33:37 +0600

    animation added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Banner from '../components/Banner';
-
+import 'animate.css';
 
 
 const Home = () => {
     return (
-        <div className='w-11/12 mx-auto mt-20 md:mt-10'>
+        <div className='w-11/12 mx-auto mt-20 md:mt-10 animate__animated animate__fadeInDown'>
             <Banner/>
         </div>
     );
