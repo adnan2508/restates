049487a7cae5952fe550879ed75444c50720454d
adnan2508 <adnandiu2800@gmail.com>
2024-04-13 02:42:49 +0600

    footer animated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='mt-10'>
+        <div className='mt-10 animate__animated animate__heartBeat'>
             <footer className="footer footer-center p-4 bg-base-300 text-base-content">
   <aside>
     <p>Copyright © 2024 - All right reserved by ReStates Ltd.</p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div>
-            <div className="navbar bg-base-100 w-11/12 mx-auto">
+            <div className="navbar bg-base-100 w-11/12 mx-auto animate__animated animate__fadeInLeft">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
