0210a1872d9b65d6df76da1da4621fee1aa2a1d5
adnan2508 <adnandiu2800@gmail.com>
2024-04-14 01:18:00 +0600

    context added

--- src/firebase/firbase.config.js
+++ src/firebase/firbase.config.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyDPPFCj-eukfyLRCbV_Gj59ecIZjn8g6uU",
   authDomain: "restates-75179.firebaseapp.com",
@@ -15,4 +13,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app;
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+export default auth;
