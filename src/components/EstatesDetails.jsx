9aae3d3a265ff865cbea09b415fd4b99b6492cfc
adnan2508 <adnandiu2800@gmail.com>
2024-04-14 14:38:42 +0600

    responsive added

--- src/components/EstatesDetails.jsx
+++ src/components/EstatesDetails.jsx
@@ -6,10 +6,9 @@ const EstatesDetails = () => {
   const { id } = useParams();
   const idInt = parseInt(id);
   const estate = estates.find((estate) => estate.id === idInt);
-  console.log(estate);
   return (
     <div>
-      <div className="w-11/12 mx-auto mt-10">
+      <div className="w-11/12 mx-auto mt-20 md:mt-10">
         <div className="card grid grid-cols-1 lg:grid-cols-2 bg-base-100 shadow-xl">
           <figure>
             <img src={estate.image} alt="estate" className="h-full rounded-3xl"/>
@@ -18,7 +17,9 @@ const EstatesDetails = () => {
           <div className="card-body flex-1">
             <h2 className="text-4xl lg:text-6xl font-bold">{estate.title}</h2>
             <div className="border-b-2 p-3"></div>
-            <p className="text-lg text-green-500 font-bold mt-3">{estate.status}</p>
+            <p className="text-xl text-green-500 font-bold mt-3">{estate.status}</p>
+            <p className="mt-3 text-lg font-semibold">Price: {estate.price}</p>
+            <p></p>
             <p className="mt-3 text-lg">{estate.description}</p>
             <p className="text-2xl font-semibold mt-3">Facilities :</p>
             <p className="text-lg">
