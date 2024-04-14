ebb71f24413e17a6f1da0b96cd55a6fa0d0d6868
adnan2508 <adnandiu2800@gmail.com>
2024-04-14 14:32:03 +0600

    property

--- src/components/EstatesDetails.jsx
+++ src/components/EstatesDetails.jsx
@@ -1,21 +1,45 @@
-import React from 'react';
-import { useLoaderData, useParams } from 'react-router-dom';
+import React from "react";
+import { Link, useLoaderData, useParams } from "react-router-dom";
 
 const EstatesDetails = () => {
-    const estates = useLoaderData();
-    const {id} = useParams();
-    const idInt = parseInt(id);
-    const estate = estates.find(estate => estate.id === idInt);
-    console.log(estate);
-    return (
-        <div>
-            <div className='w-11/12 mx-auto mt-10'>
-                <img src={estate.image} alt="" className='w-full rounded-3xl h-screen'/>
-            <h1>This is the details page of {estate.title}</h1>
+  const estates = useLoaderData();
+  const { id } = useParams();
+  const idInt = parseInt(id);
+  const estate = estates.find((estate) => estate.id === idInt);
+  console.log(estate);
+  return (
+    <div>
+      <div className="w-11/12 mx-auto mt-10">
+        <div className="card grid grid-cols-1 lg:grid-cols-2 bg-base-100 shadow-xl">
+          <figure>
+            <img src={estate.image} alt="estate" className="h-full rounded-3xl"/>
+          </figure>
+
+          <div className="card-body flex-1">
+            <h2 className="text-4xl lg:text-6xl font-bold">{estate.title}</h2>
+            <div className="border-b-2 p-3"></div>
+            <p className="text-lg text-green-500 font-bold mt-3">{estate.status}</p>
+            <p className="mt-3 text-lg">{estate.description}</p>
+            <p className="text-2xl font-semibold mt-3">Facilities :</p>
+            <p className="text-lg">
+                {estate.facilities.map(facility=><li>{facility}</li>)}
+            </p>
+            <p className="text-2xl font-semibold mt-3">Area : {estate.area}</p>
+            <p className="text-2xl font-semibold mt-3">Location : {estate.location}</p>
+
+
+            <div className="card-actions justify-end">
+              <Link to="/">
+                <button className="btn btn-warning text-white px-10">
+                  Home
+                </button>
+              </Link>
             </div>
-            
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default EstatesDetails;
